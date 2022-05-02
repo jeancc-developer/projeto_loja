d2d74e48c50d07dbb8a674eeab8085766ec2ba27
Jean Carlos <jeancc.costa@gmail.com>
2022-05-01 21:05:22 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/devjeancc/projeto_loja/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.devjeancc.projeto_loja
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Baratão da Blusas",
+
+    );
+  }
+}
